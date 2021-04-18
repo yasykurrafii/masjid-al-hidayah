6a37cf86c815ec5e7f44e70259d61ec6be6c8571
Yasykur Rafii <myasykurrafii@gmail.com>
2021-04-18 09:53:18 +0700

    Menambahkan halaman jadwal, informasi, dan laporan serta link nya

--- appmasjid/urls.py
+++ appmasjid/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('/laporan', views.laporan, name='laporan'),
+    path('/jadwal', views.jadwal, name='jadwal'),
+    path('/informasi', views.informasi, name='informasi'),
 ]
--- appmasjid/views.py
+++ appmasjid/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'content.html')
+    return render(request, 'home.html')
+
+def jadwal(request):
+    return render(request, 'jadwal.html')
+    
+def laporan(request):
+    return render(request, 'laporan.html')
+
+def informasi(request):
+    return render(request, 'informasi.html')
