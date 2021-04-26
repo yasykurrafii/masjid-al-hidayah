db7283d8736c4d92ab9d8fa23e4ab38577076d18
Yasykur Rafii <myasykurrafii@gmail.com>
2021-04-26 10:35:07 +0700

    menambahkan database untuk jadwal

--- appmasjid/models.py
+++ appmasjid/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class JadwalModel(models.Model):
+    tanggal = models.DateTimeField(auto_now=False, auto_now_add=False)
+    title = models.CharField(max_length=100)
+    penceramah = models.CharField(max_length=100)
+    online = models.BooleanField()
+
+    def __str__(self):
+        return self.title + ' | ' + self.penceramah
--- appmasjid/views.py
+++ appmasjid/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
-
+date = datetime.datetime.now()
+date = date.strftime("%A, %d %B %Y")
 def index(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'date': date})
 
 def jadwal(request):
-    return render(request, 'jadwal.html')
+    return render(request, 'jadwal.html', {'date': date})
     
 def laporan(request):
-    return render(request, 'laporan.html')
+    return render(request, 'laporan.html', {'date': date})
 
 def informasi(request):
-    return render(request, 'informasi.html')
+    return render(request, 'informasi.html', {'date': date})
