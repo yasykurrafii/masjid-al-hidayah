4e67556338b738d3186e2e7e4d525af18703ccbd
Yasykur Rafii <myasykurrafii@gmail.com>
2021-04-27 17:33:55 +0700

    add API

--- appmasjid/models.py
+++ appmasjid/models.py
@@ -5,6 +5,8 @@ from django.db import models
 
 class JadwalModel(models.Model):
     tanggal = models.DateTimeField(auto_now=False, auto_now_add=False)
+    selesai = models.TimeField(
+        auto_now=False, auto_now_add=False, default="23:00:00")
     title = models.CharField(max_length=100)
     penceramah = models.CharField(max_length=100)
     online = models.BooleanField()
--- appmasjid/views.py
+++ appmasjid/views.py
@@ -1,19 +1,65 @@
 from django.shortcuts import render
 import datetime
+import requests
 from .models import *
 
 # Create your views here.
+
+# Global Variable untuk date
 date = datetime.datetime.now()
 date = date.strftime("%A, %d %B %Y")
+
+# Function untuk fetching api
+
+
+def fetch_solat():
+    tanggal = datetime.datetime.now()
+    tanggal = tanggal.strftime("%Y-%m-%d")
+    data = requests.get(
+        "https://api.pray.zone/v2/times/day.json?city=jakarta&date=" + tanggal)
+    return data.json()
+
+# Function render
+
+
 def index(request):
+    solat = fetch_solat()
+    solat = solat['results']['datetime'][0]['times']
     jadwal = JadwalModel.objects.all()
-    return render(request, 'home.html', {'date': date, 'jadwal': jadwal})
+    return render(request, 'home.html', {'date': date,
+                                         'jadwal': jadwal,
+                                         'subuh': solat['Fajr'],
+                                         'dzuhur': solat['Dhuhr'],
+                                         'ashr': solat['Asr'],
+                                         'maghrib': solat['Maghrib'],
+                                         'isya': solat['Isha']})
+
 
 def jadwal(request):
-    return render(request, 'jadwal.html', {'date': date})
-    
+    solat = fetch_solat()
+    solat = solat['results']['datetime'][0]['times']
+    return render(request, 'jadwal.html', {'date': date, 'subuh': solat['Fajr'],
+                                           'dzuhur': solat['Dhuhr'],
+                                           'ashr': solat['Asr'],
+                                           'maghrib': solat['Maghrib'],
+                                           'isya': solat['Isha']})
+
+
 def laporan(request):
-    return render(request, 'laporan.html', {'date': date})
+    solat = fetch_solat()
+    solat = solat['results']['datetime'][0]['times']
+    return render(request, 'laporan.html', {'date': date, 'subuh': solat['Fajr'],
+                                            'dzuhur': solat['Dhuhr'],
+                                            'ashr': solat['Asr'],
+                                            'maghrib': solat['Maghrib'],
+                                            'isya': solat['Isha']})
+
 
 def informasi(request):
-    return render(request, 'informasi.html', {'date': date})
+    solat = fetch_solat()
+    solat = solat['results']['datetime'][0]['times']
+    return render(request, 'informasi.html', {'date': date, 'subuh': solat['Fajr'],
+                                              'dzuhur': solat['Dhuhr'],
+                                              'ashr': solat['Asr'],
+                                              'maghrib': solat['Maghrib'],
+                                              'isya': solat['Isha']})
