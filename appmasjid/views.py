4112ab357ee60e746f71a9e2fa60bc2bd9238f86
Yasykur Rafii <myasykurrafii@gmail.com>
2021-04-27 16:35:15 +0700

    Menambahkan code view untuk menampilkan database

--- appmasjid/views.py
+++ appmasjid/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 import datetime
+from .models import *
 
 # Create your views here.
 date = datetime.datetime.now()
 date = date.strftime("%A, %d %B %Y")
 def index(request):
-    return render(request, 'home.html', {'date': date})
+    jadwal = JadwalModel.objects.all()
+    return render(request, 'home.html', {'date': date, 'jadwal': jadwal})
 
 def jadwal(request):
     return render(request, 'jadwal.html', {'date': date})
